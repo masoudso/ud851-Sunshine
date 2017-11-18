9d39fe8553f361fb9d3380a60c4708db428ced3a
masoudso <veisi.masoud@gmail.com>
2017-11-18 15:45:56 -0500

    added some dummy weather data using an array of weather data in main activity and also modified the xml file

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,39 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // (1) Create a field to store the weather display TextView
+    // initializing weatherTextView to reference from the layout's Text view
+    private TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // (2) Use findViewById to get a reference to the weather display TextView
+        //referenced from the layout
+        weatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // (3) Create an array of Strings that contain fake weather data
+
+        String[] TehranWeather = {
+                "Today, Nov 18 - Clear - 20 C / 15 C",
+                "Tomorrow, Nov 19 - Cloudy - 21 C / 18 C",
+                "Monday, Nov 20 - Rainy - 18 C / 14 C",
+                "Tuesday, Nov 21 - Cloudy - 20 C / 15C",
+                "Wednesday, Nov 22 - Sunny - 20 C / 18C",
+                "Thursday, Nov 23 - Rainy - 19 C / 16C",
+                "Friday, Nov 24 - Clear - 20 C / 15C",
+        };
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        weatherTextView.append("Tehran Wather\n\n");
+        for (String TehranWeatherData : TehranWeather){
+        weatherTextView.append(TehranWeatherData + "\n\n\n");
+        }
     }
 }
